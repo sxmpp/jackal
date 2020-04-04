77e01528d2f32e7b4e3f4393c51f21f3678c7cf7
ortuman <ortuman@gmail.com>
2020-04-04 21:08:47 +0200

    add cluster memberlist

--- cluster/router/router.go
+++ cluster/router/router.go
@@ -13,13 +13,14 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/sony/gobreaker"
+	"github.com/ortuman/jackal/util/pool"
 
 	"github.com/ortuman/jackal/cluster"
 	"github.com/ortuman/jackal/log"
 	"github.com/ortuman/jackal/router"
 	"github.com/ortuman/jackal/storage"
 	"github.com/ortuman/jackal/xmpp"
+	"github.com/sony/gobreaker"
 	"golang.org/x/net/http2"
 )
 
@@ -31,6 +32,7 @@ type clusterRouter struct {
 	presencesSt storage.Presences
 	hClient     *http.Client
 	cb          *gobreaker.CircuitBreaker
+	pool        *pool.BufferPool
 }
 
 func New(cluster *cluster.Cluster, presencesSt storage.Presences) (router.ClusterRouter, error) {
@@ -46,6 +48,7 @@ func New(cluster *cluster.Cluster, presencesSt storage.Presences) (router.Cluste
 		presencesSt: presencesSt,
 		hClient:     &http.Client{Transport: h2cTransport},
 		cb:          gobreaker.NewCircuitBreaker(gobreaker.Settings{}),
+		pool:        pool.NewBufferPool(),
 	}
 	if err := r.leader.Elect(); err != nil {
 		return nil, err
@@ -74,7 +77,33 @@ func (r *clusterRouter) Route(ctx context.Context, stanza xmpp.Stanza) error {
 }
 
 func (r *clusterRouter) route(ctx context.Context, stanza xmpp.Stanza, member *cluster.Member) error {
-	return nil
+	buf := r.pool.Get()
+	defer r.pool.Put(buf)
+
+	if err := stanza.ToXML(buf, true); err != nil {
+		return err
+	}
+	url := fmt.Sprintf("http://%s:%s/route", member.Host, member.Port)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, buf)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/xml")
+
+	_, err = r.cb.Execute(func() (i interface{}, e error) {
+		resp, err := r.hClient.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer func() { _ = resp.Body.Close() }()
+
+		if resp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("response status code: %d", resp.StatusCode)
+		}
+		return nil, nil
+	})
+	return err
 }
 
 func (r *clusterRouter) loop() {
